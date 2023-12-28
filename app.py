f4f2b1b1ed255e7bd1f64972d783d1935dc49bdd
Hidetaka-Konishi <apth010f@gmail.com>
2023-12-28 11:30:45 +0900

    ローカルストレージから値を読み込むJavaScriptコードを追加し、その値をStreamlitのウィジェットにセット

--- app.py
+++ app.py
@@ -4,26 +4,33 @@ import streamlit.components.v1 as components
 # Streamlitのページ設定
 st.set_page_config(page_title="JavaScript Test")
 
+# キー名を定義
+key_name = 'user_input'
+
+# ローカルストレージから値を読み込む
+if 'load_once' not in st.session_state:
+    components.html(f"""
+        <script>
+            window.onload = () => {{
+                const savedValue = localStorage.getItem('{key_name}');
+                if (savedValue) {{
+                    window.parent.postMessage({{
+                        type: 'streamlit:set_widget_value',
+                        key: '{key_name}',
+                        value: savedValue
+                    }}, '*');
+                }}
+            }}
+        </script>
+    """, height=0)
+    st.session_state.load_once = True
+
 # ユーザーからの入力を取得
-text = st.text_input("文字を入力")
+text = st.text_input("文字を入力", key=key_name)
 
-# JavaScriptコードを含むHTMLを定義
-html_code = f"""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>JavaScript Test</title>
-</head>
-<body>
+# 入力された値をローカルストレージに保存
+components.html(f"""
     <script>
-        // ローカルストレージにデータを保存
-        localStorage.setItem('キー', '{text}');
-        var value = localStorage.getItem('キー');
-        console.log(value);
+        localStorage.setItem('{key_name}', '{text}');
     </script>
-</body>
-</html>
-"""
-
-# StreamlitアプリにHTMLコードを組み込む
-components.html(html_code, height=100)
+""", height=0)
